aeecf557c7f8c8b51be2ce22bcc78f2687609ce8
Elaine Cristina dos Santos <elainesantos.pessoal18@gmail.com>
2025-04-27 01:08:27 -0300

    update

---
+++ Calculadora/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+Console.WriteLine("Olá, seja bem-vindo a calculadora.");
+Console.WriteLine("----------------------------------");
+Console.WriteLine("Digite a opção que deseja executar: ");
+Console.WriteLine("[1] Soma");
+Console.WriteLine("[2] Subtração");
+Console.WriteLine("[3] Multiplicação");
+Console.WriteLine("[4] Divisão");
+Console.WriteLine("[0] Sair");
+
+int opcaoEscolhida = int.Parse(Console.ReadLine());
+
+switch (opcaoEscolhida)
+{
+    case 0:
+        Console.WriteLine("Você escolheu sair.");
+        break;
+    case 1:
+        Console.WriteLine("A opção escolhida foi: Soma");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("Digite o primeiro número: ");
+        double numero1_Soma = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número: ");
+        double numero2_Soma = double.Parse(Console.ReadLine());
+
+        double resultadoSoma = numero1_Soma + numero2_Soma;
+        Console.WriteLine("O valor da soma dos dois números informados equivale a: " + resultadoSoma);
+
+        break;
+    case 2:
+        Console.WriteLine("A opção escolhida foi: Subtração");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("Digite o primeiro número: ");
+        double numero1_Subtracao = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número: ");
+        double numero2_Subtracao = double.Parse(Console.ReadLine());
+
+        double resultadoSubtracao = numero1_Subtracao - numero2_Subtracao;
+        Console.WriteLine("O valor da subtração dos dois numeros informados equivale a: " + resultadoSubtracao);
+
+        break;
+    case 3:
+        Console.WriteLine("A opção escolhida foi: Multiplicação");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("Digite o primeiro número: ");
+        double numero1_multiplicacao = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número: ");
+        double numero2_multiplicacao = double.Parse(Console.ReadLine());
+
+        double resultadoMultiplicacao = numero1_multiplicacao * numero2_multiplicacao;
+        Console.WriteLine("O valor da multiplicação dos dois numeros informados equivale a: " + resultadoMultiplicacao);
+
+        break;
+    case 4:
+        Console.WriteLine("A opção escolhida foi: Divisão");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("Digite o primeiro número: ");
+        double numero1_divisao = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número: ");
+        double numero2_divisao = double.Parse(Console.ReadLine());
+
+        if (numero1_divisao == 0 || numero2_divisao == 0)
+        {
+            Console.WriteLine("Não é possível dividir por zero.");
+        }
+        else
+        {
+
+            double resultadoDivisao = numero1_divisao / numero2_divisao;
+            Console.WriteLine("O valor da divisão dos dois numeros informados equivale a: " + resultadoDivisao);
+        }
+        break;
+}
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # CalculadoraSimples_Csharp
 Objetivo: usar uma aplicação do tipo console do dotnet, criar uma calculadora simples que terá como operações de soma, subtração, multiplicação e divisão. 
+
+## Funcionalidades desse exercício
+
+Nossa primeira aplicação é bem simples e, se você já tem conhecimentos em lógica de programação, algoritmos e alguma linguagem de programação não terá problemas para desenvolver a aplicação.
+Faremos uso de operadores condicionais, estrutura condicional 'switch', criação e chamada de métodos simples.
+
+## Requisitos para desenvolver a aplicação
+
+- Ao entrar na calculadora o usuário deverá ter um menu com as opções 1, 2, 3, 4 e 0. Sendo elas:
+
+	1 - Soma
+	
+	2 - Subtração
+	
+	3 - Multiplicação
+	
+	4 - Divisão
+	
+	0 - Sair
+	
+
+- Todas as operações devem solicitar que o usuário entre com dois valores e então estes valores devem ser utilizados para realizar a operação escolhida. Exemplo de fluxo da operação de Soma:
+	1. Aplicação solicita o primeiro valor;
+	2. Usuário digita valor e pressiona enter;
+	3. Aplicação solicita o segundo valor;
+	4. Usuário digita valor e pressiona enter;
+	5. Aplicação escreve em tela o resultado da soma;
+	6. Usuário pressiona Enter;
+	7. Aplicação será encerrada;
+
+- Para a operação de divisão, validar se o divisor é 0. Se verdadeiro deve ser exibida a mensagem "Não é possível dividir por zero."
